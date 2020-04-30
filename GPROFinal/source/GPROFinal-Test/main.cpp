bc86703aecedec06c92b39c7e11eccc3fcb51f97
Tyler Sloss <tyler.sloss@mymail.champlain.edu>
2020-04-30 14:34:16 -0400

    FIXED THE PROJECT FINALLY

--- GPROFinal/source/GPROFinal-Test/main.cpp
+++ GPROFinal/source/GPROFinal-Test/main.cpp
@@ -1,6 +1,9 @@
-#include "GPROFinal/GPROFinal.h"
+#include "GPROFinal/GPROFinal.h"+
+#include <iostream>
+using namespace std;
 int main()
 {
 	int f, bar = 2;
 	f = foo(bar);
+	cout << f << endl;
 }
---
+++ GPROFinal/source/GPROFinal/GPROFinal.cpp
@@ -0,0 +1,7 @@
+#include "GPROFinal.h"
+int foo(int bar)
+{
+	int foo;
+	foo = bar * 2;
+	return foo;
+}
